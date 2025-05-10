c2012c77a45e1a6b3061e306f4e0e14c9034c1de
AritraBose-cloud <rajbose.barasat@gmail.com>
2025-05-10 16:44:53 +0000

    Saved

---
+++ Basics/variables_decla.js
@@ -0,0 +1,13 @@
+const accountno = 2456
+let accountEmail = "aritrabose.@gmail.com"
+ var accountCity = "Barasat"
+
+ console.table([accountno, accountEmail, accountCity])
+
+ /*accountno = "456"*/ //Const variables cannot be changed
+ accountEmail = "Mahuabose.@gmail.com"
+ accountCity = "Kolkata"
+
+ console.log("After changing");
+ console.table([accountno, accountEmail, accountCity])
+ 
