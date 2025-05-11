46072609c737a1753230553caa93292845298558
AritraBose-cloud <rajbose.barasat@gmail.com>
2025-05-11 06:30:24 +0000

    Saved

---
+++ Basics/Comparison.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+
+// console.log("2" > 1) 
+// console.log("2" == 1);
+// During comparision Datatypes shoule be same
+
+console.log(" \n During comparing null datatype");
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);//It shows true because in >= operator converts the null to 0
+
+//In "undefined" datatype any type of comparision will show the output as 0
+
+// Strict check (===)
+
+console.log("2" === 2);// Checks the datatype while comparision
---
+++ Basics/Operations.js
@@ -0,0 +1,17 @@
+let value = 120;
+let negvalue = -value;
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " Aritra"
+let str3 = str1 + str2
+console.log(str3);
+console.log(str1 + str2);
+// Any both can be done 
+
+console.log("1" + 2);// Shown in String output
+console.log(1 + "2");// Also shown in string output
+console.log("1" + 2 + 2);// String output
+console.log( 1 + 2 + "2");//First 1 and 2 are added then string output
+
+
